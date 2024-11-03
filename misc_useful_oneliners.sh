0597e1e0e10aa48eda688a379aae32a7cc2f4888
Amalia M. <a.mailli@yahoo.com>
2024-11-03 16:20:10 +0200

    Create misc_useful_oneliners.sh
    An assortment of different bash one liner commands I keep forgetting but can be useful.

---
+++ misc_useful_oneliners.sh
@@ -0,0 +1,4 @@
+## An assortment of different one liner commands: 
+
+##Starting from the current directory, find all files ending with X. In the example below, all bash files ("sh")
+   find . -type f -name \*sh
